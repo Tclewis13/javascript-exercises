b3aade0e0f7aaf31bd851732be7de5f6cd5c43d3
Tclewis13 <the3rdi@gmail.com>
2024-08-26 02:33:16 -0400

    seventh exercise done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(inputDegrees) {
+  let convertedDegrees;
+  convertedDegrees = ((inputDegrees-32)*5)/9;
+  convertedDegrees = Math.round(convertedDegrees * 10) / 10;
+  return convertedDegrees;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(inputDegrees) {
+  let convertedDegrees;
+  convertedDegrees = ((inputDegrees*9)/5)+32;
+  convertedDegrees = Math.round(convertedDegrees * 10) / 10;
+  return convertedDegrees;
 };
 
 // Do not edit below this line
