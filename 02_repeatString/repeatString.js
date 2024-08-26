0e8c1f6c8d7189ed2a1c316a1224cf7a0d616a18
Tclewis13 <the3rdi@gmail.com>
2024-08-25 20:08:39 -0400

    second exercise done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
-};
+const repeatString = function(inputString, loops) {
+    let returnString = '';
+    if(loops < 0){
+        return 'ERROR';
+    }
+    else{
+        for(i=0; i < loops; i++){
+            returnString = returnString + inputString;
+        }
+        return returnString;
+    };
+}
+    
 
 // Do not edit below this line
 module.exports = repeatString;
