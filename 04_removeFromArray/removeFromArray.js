2a88c1397af2990ac92d1cdc13d6145042d7af23
Tclewis13 <the3rdi@gmail.com>
2024-08-25 22:01:38 -0400

    fourth exercise done. look its kind of gross but it works

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,35 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    let inputArray = args[0]
+    args.shift();
+    let workingArray = [...inputArray];
+    let elementRemoved = true;
+    for (let i = 0; i < args.length; i++){
+        while ( elementRemoved === true){
+            elementRemoved = false;
+            let workingLength = workingArray.length;
+            workingArray = removeElement(workingArray, args[i]);
+            if(workingLength > workingArray.length){
+                elementRemoved = true;
+            }
+        }
+        elementRemoved = true;
+        
+    }
+    return workingArray;
+
+
+    function removeElement(inputArray, removal){
+        let returnArray = [...inputArray];
+        console.log('Looking for ' + removal);
+        returnArray.forEach(element => {
+            if(element === removal){
+                returnArray.splice(inputArray.indexOf(element),1);
+            }
+        });
+
+        return returnArray;
+    }
+   
 };
 
 // Do not edit below this line
