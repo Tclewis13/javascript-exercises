bf871eaef2d9f606c9c7a83d93730e249ca75dc9
Tclewis13 <the3rdi@gmail.com>
2024-08-25 22:26:13 -0400

    exercise five done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,32 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    let smallerNum;
+    let biggerNum;
+    let sum = 0;
 
+    if( firstNum < 0 || secondNum < 0){
+        return 'ERROR';
+    }
+    if (Number.isInteger(firstNum) === false || Number.isInteger(secondNum) === false){
+        return 'ERROR';
+    }
+
+    if(firstNum < secondNum){
+        smallerNum = firstNum;
+        biggerNum = secondNum;
+    }
+    else if(firstNum > secondNum){
+        smallerNum = secondNum;
+        biggerNum = firstNum;
+    }
+    else if (firstNum === secondNum){
+        return firstNum;
+    }
+
+    for(i = smallerNum; i <= biggerNum; i++){
+        sum+=i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
