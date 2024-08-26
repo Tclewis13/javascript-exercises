d19ba32c7c3ca8514b7e24b7e0d30189b517131e
Tclewis13 <the3rdi@gmail.com>
2024-08-26 01:40:16 -0400

    sixth exercise done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,23 @@
-const leapYears = function() {
+const leapYears = function(inputYear) {
 
-};
+    if((inputYear % 4) === 0){
+        if((inputYear % 100) === 0){
+            if((inputYear % 400) === 0){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else{
+            return true;
+        }
+        
+    }
+    else{
+        return false;
+    }
+}
 
 // Do not edit below this line
 module.exports = leapYears;
