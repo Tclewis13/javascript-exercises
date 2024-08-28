f4fefaaf5e28d0250c846fe3b119fd470273a737
Tclewis13 <the3rdi@gmail.com>
2024-08-28 18:50:32 -0400

    eighth exercise done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(firstNum, secondNum) {
+  return firstNum + secondNum;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNum, secondNum) {
+	return firstNum - secondNum;
 };
 
-const sum = function() {
-	
+const sum = function(inputArray) {
+	const summation = inputArray.reduce((total, currentItem) => {
+    return total + currentItem;
+  }, 0);
+  return summation;
 };
 
-const multiply = function() {
-
+const multiply = function(inputArray) {
+  const summation = inputArray.reduce((total, currentItem) => {
+    return total * currentItem;
+  }, 1);
+  return summation;
 };
 
-const power = function() {
-	
+const power = function(firstNum, secondNum) {
+	return Math.pow(firstNum,secondNum);
 };
 
-const factorial = function() {
-	
+const factorial = function(inputNum) {
+	let summation = 1;
+  if (inputNum === 0 || inputNum === 1){
+    return 1;
+  }
+  else{
+    for (i = inputNum; i > 0; i--){
+      summation *= i;
+      console.log(summation);
+    }
+    return summation;
+  }
 };
 
 // Do not edit below this line
