9273b5c7dd49640ccf19083f2a2f6ca2955f8ddf
Tclewis13 <the3rdi@gmail.com>
2024-08-28 19:40:17 -0400

    ninth exercise done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,41 @@
-const palindromes = function () {
-
+const palindromes = function (inputString) {
+    let normalString = inputString;
+    normalString = removePunctuation(normalString);
+    normalString = removeSpaces(normalString);
+    let reversedString = reverseString(normalString);
+    normalString = normalString.toLowerCase();
+    reversedString = reversedString.toLowerCase();
+    if (normalString === reversedString){
+        console.log(normalString);
+        console.log(reversedString);
+        return true;
+    }
+    else{
+        console.log(normalString);
+        console.log(reversedString);
+        return false;
+    }
 };
+
+function reverseString(inputString) {
+    let stringArray = inputString.split("");
+
+    stringArray = stringArray.reverse();
+
+    stringArray = stringArray.join("");
+
+    return stringArray;
+};
+
+function removePunctuation(str) {
+    str = str.replace(/(~|`|!|@|#|$|%|^|&|\*|\(|\)|{|}|\[|\]|;|:|\"|'|<|,|\.|>|\?|\/|\\|\||-|_|\+|=)/g,"")
+    return str;
+}
+
+function removeSpaces(str){
+    str = str.replace(/\s+/g, '');
+    return str;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
