a22df5155333fc09b1a3160abe04a3b103466bdc
Tclewis13 <the3rdi@gmail.com>
2024-08-29 00:14:35 -0400

    twelfth exercise done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,36 @@
-const findTheOldest = function() {
+const findTheOldest = function(inputArray) {
 
+    const oldest = inputArray.reduce((workingOldest, currentItem) => {
+        let currentAge;
+        let workingAge;
+        if (workingOldest.yearOfDeath === undefined){
+            let today = new Date();
+            today = today.getFullYear();
+            workingAge = today - workingOldest.yearOfBirth;
+            //console.log(workingOldest.name);
+            //console.log(workingAge);
+        }
+        else{
+            workingAge = workingOldest.yearOfDeath - workingOldest.yearOfBirth;
+        }
+        if (currentItem.yearOfDeath === undefined){
+            let today = new Date();
+            today = today.getFullYear();
+            currentAge = today - currentItem.yearOfBirth;
+            //console.log(currentItem.name);
+            //console.log(currentAge);
+        }
+        else{
+            currentAge = currentItem.yearOfDeath - currentItem.yearOfBirth;
+        }
+        if(currentAge > workingAge){
+            return currentItem;
+        }
+        else{
+            return workingOldest;
+        }
+    });
+    return oldest;
 };
 
 // Do not edit below this line
