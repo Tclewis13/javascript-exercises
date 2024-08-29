ed9c051869110009e63ae3f500acacca06900cdc
Tclewis13 <the3rdi@gmail.com>
2024-08-28 23:38:02 -0400

    eleventh exercise done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    const mappedArr = books.map(getOneTitle);
+    return mappedArr;
+}
 
-};
+
+function getOneTitle(book){
+   return book.title 
+}
 
 // Do not edit below this line
 module.exports = getTheTitles;
